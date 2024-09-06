2f362eaf0937360a0f2a81bac41be81111fa06a8
Valentin Popov <valentin@popov.link>
2024-09-06 08:21:27 +0000

    Added reading time timer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/plugins/remarkReadingTime";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://popov.link",
 	integrations: [sitemap()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
 });
