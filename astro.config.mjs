43f80a7b6ee32f721b5685a206cae78f9f625a12
Valentin Popov <valentin@popov.link>
2024-09-17 21:31:23 +0000

    Update dependencies and add Cloudflare adapter in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/plugins/remarkReadingTime";
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://popov.link",
+	output: "hybrid",
+	adapter: cloudflare(),
 	integrations: [sitemap()],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
