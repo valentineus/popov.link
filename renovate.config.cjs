2b4908aa922cc5656a964305d48cc94a6b3fe00a
Valentin Popov <valentin@popov.link>
2025-05-08 13:12:45 +0000

    Added self-hosted Renovate Bot

---
+++ renovate.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+	endpoint: "https://code.popov.link",
+	gitAuthor: "RenovateBot <renovatebot@noreply.localhost>",
+	onboardingConfigFileName: ".gitea/renovate.json",
+	optimizeForDisabled: true,
+	platform: "gitea",
+};
